ccff0291c14831af5f7833454822a0617ec0420f
小山内美悠 <miumiu917@gmail.com>
2019-07-25 21:07:30 +0900

    updated

--- web/wp-config.php
+++ web/wp-config.php
@@ -4,8 +4,6 @@
  * This file is required in the root directory so WordPress can find it.
  * WP is hardcoded to look in its own directory or one directory up for wp-config.php.
  */
-define( 'AS3CF_AWS_ACCESS_KEY_ID',     'AKIA5EOOVWLODNPJRWV6' );
-define( 'AS3CF_AWS_SECRET_ACCESS_KEY', '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' );
 require_once(dirname(__DIR__) . '/vendor/autoload.php');
 require_once(dirname(__DIR__) . '/config/application.php');
 require_once(ABSPATH . 'wp-settings.php');
