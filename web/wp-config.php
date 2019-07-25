09038a214f2710dfa80a42d12ea75b9870236be8
小山内美悠 <miumiu917@gmail.com>
2019-07-25 21:19:16 +0900

    udpated

--- web/wp-config.php
+++ web/wp-config.php
@@ -4,8 +4,8 @@
  * This file is required in the root directory so WordPress can find it.
  * WP is hardcoded to look in its own directory or one directory up for wp-config.php.
  */
-define( 'AS3CF_AWS_ACCESS_KEY_ID',     $_ENV['ACCESS_KEY'] );
-define( 'AS3CF_AWS_SECRET_ACCESS_KEY', $_ENV['SECRET_KEY'] );
+define( 'AS3CF_AWS_ACCESS_KEY_ID',     getenv('ACCESS_KEY') );
+define( 'AS3CF_AWS_SECRET_ACCESS_KEY', getenv('SECRET_KEY') );
 require_once(dirname(__DIR__) . '/vendor/autoload.php');
 require_once(dirname(__DIR__) . '/config/application.php');
 require_once(ABSPATH . 'wp-settings.php');
